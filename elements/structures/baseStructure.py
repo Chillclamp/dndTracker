7b6083419592f5ba4d63941490903a55bffac6dd
chillclamp <adamdgh888@gmail.com>
2025-07-04 14:59:49 +0100

    inital structure object

--- elements/structures/baseStructure.py
+++ elements/structures/baseStructure.py
@@ -7,3 +7,56 @@ class baseStructure:
     def describe(self):
         return (f"{self.name} located at {self.location}, size: {self.size}, "
                 f"built with: {', '.join(self.materials)}.")
+
+
+class settlemant(baseStructure):
+    def __init__(self, name, location, materials):
+        super().__init__(name, location, materials)
+
+
+
+########################################
+# Settlement
+# Strucure [REcreaton]
+
+# method:
+#    make struct(an amount)
+#       loped
+#       random num(0-100)
+#           if num < 50 
+#               make stall class
+#           elif num 50 - 90:
+#               make shop class
+#           else:
+#               make market class
+#       add new class to struct's struct list
+
+
+#########################################
+# Trade - Stall
+# pick trade class(s) form random?
+# random pick num of trades
+# for loop
+# pull item from database where relevent trade class
+# num of items 
+#    method -> item traded 
+#   method -> restock
+
+
+#######################################
+# # CHARS
+#######################################
+# Base class
+# name
+# 
+
+# elven class
+#       wood elf
+#       elder elf
+
+# dwarf 
+
+# CHAR PROFESIUON
+# trader
+# 
+# 
